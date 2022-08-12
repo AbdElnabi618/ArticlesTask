37746c10c45636343ef4f45b2b249de8a1e3e0f0
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-12 19:24:36 +0200

    - add di annotation

--- app/src/main/java/com/abdelnabi618/articlestask/di/ArticlesModule.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/di/ArticlesModule.kt
@@ -1,17 +1,20 @@
 package com.abdelnabi618.articlestask.di
 
 import com.abdelnabi618.articlestask.data.remote.ArticlesApiCall
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 class ArticlesModule {
-
-
 
     @Provides
     @Singleton
-    fun provideArticlesCall(retrofit: Retrofit) : ArticlesApiCall =
+    fun provideArticlesCall(retrofit: Retrofit): ArticlesApiCall =
         retrofit.create(ArticlesApiCall::class.java)
 
 
