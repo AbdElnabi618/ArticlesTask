c04508e91a57d8c2ffb0674e736d6766738817d9
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-12 17:29:22 +0200

    adding splash screen

--- app/src/main/java/com/abdelnabi618/articlestask/ui/activities/MainAppActivityActivity.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/ui/activities/MainAppActivityActivity.kt
@@ -3,9 +3,9 @@ package com.abdelnabi618.articlestask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainAppActivityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_app)
     }
 }
