3048ce248841ebbb0b613b5d2af247e667631301
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-12 18:17:19 +0200

    - add articles model

---
+++ app/src/main/java/com/abdelnabi618/articlestask/model/ArticlesModel.kt
@@ -0,0 +1,38 @@
+package com.abdelnabi618.articlestask.model
+
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "Articles")
+data class ArticlesModel(
+    @ColumnInfo(name = "body")
+    @SerializedName("body")
+    val body: String,
+
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @SerializedName("id")
+    @ColumnInfo(name = "apiId")
+    val apiId: Int,
+
+    @SerializedName("reactions")
+    @ColumnInfo(name = "reactions")
+    val reactions: Int,
+
+    @SerializedName("tags")
+    @ColumnInfo(name = "tags")
+    val tags: List<String>,
+
+    @SerializedName("title")
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @SerializedName("userId")
+    @ColumnInfo(name = "userId")
+    val userId: Int
+)
