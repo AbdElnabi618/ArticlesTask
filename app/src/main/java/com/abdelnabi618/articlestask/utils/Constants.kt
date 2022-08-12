df8cd63d70f6af8cb7b62adf4c5abfcc90f76351
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-12 18:34:30 +0200

    - add database names constants

---
+++ app/src/main/java/com/abdelnabi618/articlestask/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.abdelnabi618.articlestask.utils
+
+object Constants {
+
+    const val DATABASE_NAME = "articles_db"
+    const val DATABASE_VERSION = 1
+
+    const val ARTICLES_TABLE_NAME = "Articles"
+}
