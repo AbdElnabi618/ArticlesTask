fe0215f8ea55fd9734e8c87070bf6284b69c86af
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-13 21:08:01 +0200

    - add all article injection methods

--- app/src/main/java/com/abdelnabi618/articlestask/di/ArticlesModule.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/di/ArticlesModule.kt
@@ -1,6 +1,13 @@
 package com.abdelnabi618.articlestask.di
 
+import com.abdelnabi618.articlestask.data.data_source.articles_paging.ArticlesRemoteMediator
+import com.abdelnabi618.articlestask.data.data_source.articles.ArticlesDataSource
+import com.abdelnabi618.articlestask.data.data_source.articles.ArticlesDataSourceImpl
+import com.abdelnabi618.articlestask.data.local.room.ArticlesDao
+import com.abdelnabi618.articlestask.data.local.room.ArticlesDatabase
 import com.abdelnabi618.articlestask.data.remote.ArticlesApiCall
+import com.abdelnabi618.articlestask.repository.ArticlesRepository
+import com.abdelnabi618.articlestask.repository.ArticlesRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -17,5 +24,28 @@ class ArticlesModule {
     fun provideArticlesCall(retrofit: Retrofit): ArticlesApiCall =
         retrofit.create(ArticlesApiCall::class.java)
 
+    @Provides
+    @Singleton
+    fun provideArticlesDataSource(apiCall: ArticlesApiCall): ArticlesDataSource =
+        ArticlesDataSourceImpl(apiCall)
 
+
+    @Provides
+    @Singleton
+    fun provideArticleRemoteMediator(
+        articlesDatabase: ArticlesDatabase,
+        articlesDataSource: ArticlesDataSource
+    ): ArticlesRemoteMediator =
+        ArticlesRemoteMediator(
+            articlesDatabase,
+            articlesDataSource
+        )
+
+    @Provides
+    @Singleton
+    fun provideArticlesRepository(
+        articlesRoomDao: ArticlesDao,
+        articlesRemoteMediator: ArticlesRemoteMediator
+    ): ArticlesRepository =
+        ArticlesRepositoryImpl(articlesRoomDao, articlesRemoteMediator)
 }
