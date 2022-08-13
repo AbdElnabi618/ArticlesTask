c21a71a59e6f303bf01291db7e92ed29b19c0b00
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-14 00:24:01 +0200

    - add cashed in to flow

--- app/src/main/java/com/abdelnabi618/articlestask/ui/fragments/articles_list/ArticlesViewModel.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/ui/fragments/articles_list/ArticlesViewModel.kt
@@ -1,21 +1,21 @@
 package com.abdelnabi618.articlestask.ui.fragments.articles_list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.abdelnabi618.articlestask.model.ArticlesModel
 import com.abdelnabi618.articlestask.repository.ArticlesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class ArticlesViewModel @Inject constructor(
     private val articlesRepository: ArticlesRepository
-): ViewModel() {
+) : ViewModel() {
 
 
-
-    suspend fun getAllArticles(): Flow<PagingData<ArticlesModel>> = articlesRepository.getArticles()
+    suspend fun getAllArticles(): Flow<PagingData<ArticlesModel>> =
+        articlesRepository.getArticles().cachedIn(viewModelScope)
 }
