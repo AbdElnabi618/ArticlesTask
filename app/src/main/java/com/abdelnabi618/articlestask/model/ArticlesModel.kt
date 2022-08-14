1d3c025f47609594e8afdfc35bee9126a01523ba
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-14 05:24:52 +0200

    - remove unnecessary annotations

--- app/src/main/java/com/abdelnabi618/articlestask/model/ArticlesModel.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/model/ArticlesModel.kt
@@ -7,36 +7,23 @@ import androidx.room.PrimaryKey
 import androidx.room.TypeConverters
 import com.abdelnabi618.articlestask.utils.Constants.ARTICLES_TABLE_NAME
 import com.abdelnabi618.articlestask.utils.ListToStringConverters
-import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = ARTICLES_TABLE_NAME)
 @TypeConverters(ListToStringConverters::class)
 data class ArticlesModel(
     @ColumnInfo(name = "body")
-    @SerializedName("body")
     val body: String,
-
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "databaseId")
     val id: Int,
-
-    @SerializedName("id")
     @ColumnInfo(name = "apiId")
     val apiId: Int,
-
-    @SerializedName("reactions")
     @ColumnInfo(name = "reactions")
     val reactions: Int,
-
-    @SerializedName("tags")
     @ColumnInfo(name = "tags")
     val tags: List<String>,
-
-    @SerializedName("title")
     @ColumnInfo(name = "title")
     val title: String,
-
-    @SerializedName("userId")
     @ColumnInfo(name = "userId")
     val userId: Int
 )
