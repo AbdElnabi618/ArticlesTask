07761140849913af6851bc09a804a66d20f9eb01
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-14 03:01:39 +0200

    - get data from view model

--- app/src/main/java/com/abdelnabi618/articlestask/ui/fragments/articles_details/ArticleDetailsFragment.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/ui/fragments/articles_details/ArticleDetailsFragment.kt
@@ -4,16 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.abdelnabi618.articlestask.databinding.FragmentArticleDetailsBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
-
+@AndroidEntryPoint
 class ArticleDetailsFragment : Fragment() {
 
     private val articleId: Int by lazy {
         navArgs<ArticleDetailsFragmentArgs>().value.articleId
     }
+
+    private val articleDetailsViewModel : ArticleDetailsViewModel by viewModels()
 
     private val articleDetailsFragmentBinding: FragmentArticleDetailsBinding by lazy {
         FragmentArticleDetailsBinding.inflate(layoutInflater)
@@ -29,6 +36,27 @@ class ArticleDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launchWhenResumed {
+
+            articleDetailsViewModel.loadingStateFlow.collectLatest {
+                articleDetailsFragmentBinding.articleDetailsLoadingPb.isVisible = it
+            }
+
+            articleDetailsViewModel.articleStateFlow.collectLatest {
+                articleDetailsFragmentBinding.articleModel = it
+            }
+
+            articleDetailsViewModel.getSingleArticle(articleId)
+        }
+
+        onEvent()
+    }
+
+    private fun onEvent() {
+        articleDetailsFragmentBinding.articleDetailsBackImg.setOnClickListener {
+            requireActivity().onBackPressed()
+        }
     }
 
 }
