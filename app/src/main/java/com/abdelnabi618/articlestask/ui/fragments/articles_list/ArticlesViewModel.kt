a05a02a758f02f2f4beff5f3ae92529a2d3e8d56
kh618 <khaled.abdelnabi.18@gmail.com>
2022-08-14 05:53:15 +0200

    - set flow as member variable

--- app/src/main/java/com/abdelnabi618/articlestask/ui/fragments/articles_list/ArticlesViewModel.kt
+++ app/src/main/java/com/abdelnabi618/articlestask/ui/fragments/articles_list/ArticlesViewModel.kt
@@ -7,7 +7,9 @@ import androidx.paging.cachedIn
 import com.abdelnabi618.articlestask.model.ArticlesModel
 import com.abdelnabi618.articlestask.repository.ArticlesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.emitAll
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,10 +17,16 @@ class ArticlesViewModel @Inject constructor(
     private val articlesRepository: ArticlesRepository
 ) : ViewModel() {
 
+    private val _articlesFlow: MutableSharedFlow<PagingData<ArticlesModel>> = MutableSharedFlow()
+    val articlesFlow: SharedFlow<PagingData<ArticlesModel>> = _articlesFlow
 
-    suspend fun getAllArticles(): Flow<PagingData<ArticlesModel>> =
-        articlesRepository.getArticles().cachedIn(viewModelScope)
+    suspend fun getAllArticles() {
+        _articlesFlow.emitAll(articlesRepository.getArticles().cachedIn(viewModelScope))
+    }
 
-    suspend fun filterArticles(articleId: Int): Flow<PagingData<ArticlesModel>> =
-        articlesRepository.filterArticles(articleId).cachedIn(viewModelScope)
+
+    suspend fun filterArticles(articleId: Int) {
+        _articlesFlow.emitAll(articlesRepository.filterArticles(articleId).cachedIn(viewModelScope))
+    }
+
 }
